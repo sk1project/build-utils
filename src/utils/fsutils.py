509944aab9f86cbed7c94dbac0bbc9175ce057ff
sk1project <sk1.project.org@gmail.com>
2020-08-26 03:56:09 +0300

    refactoring for py3

--- src/utils/fsutils.py
+++ src/utils/fsutils.py
@@ -16,113 +16,115 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import glob
 import os
 import shutil
+import typing as tp
 
 
-def get_filenames(path='.', ext='*'):
-    """
-    Returns filename list for provided path filtering by extension.
-    """
-    result = []
-    names = []
-    if path:
-        if os.path.isdir(path):
-            try:
-                names = os.listdir(path)
-            except os.error:
-                return []
-        names.sort()
-        for name in names:
-            if not os.path.isdir(os.path.join(path, name)):
-                if ext == '*':
-                    result.append(name)
-                elif name.endswith('.' + ext):
-                    result.append(name)
-    return result
+def get_filenames(path: str = '.', ext: str = '*') -> tp.List[str]:
+    """Returns filename list for provided path filtering by extension
 
-
-def get_filepaths(path='.', ext='*'):
-    """
-    Returns file path list for provided path filtering by extension.
-    """
-    import glob
-    file_items = glob.glob(os.path.join(path, "*." + ext))
-    file_items.sort()
-    result = []
-    for file_item in file_items:
-        if os.path.isfile(file_item):
-            result.append(file_item)
-    return result
-
-
-def get_dirpaths(path='.'):
-    """
-    Return directory path list for provided path
+    :param path: (str) working directory, default - current directory
+    :param ext: (str) select by the extension (e.g. 'svg', 'ai' etc.), default - any extension
+    :return: (list) found file names list
     """
     result = []
-    names = []
     if os.path.isdir(path):
         try:
-            names = os.listdir(path)
+            for name in sorted(os.listdir(path)):
+                if os.path.isfile(os.path.join(path, name)):
+                    if ext == '*' or name.endswith(f'.{ext}'):
+                        result.append(name)
         except os.error:
-            return names
-    names.sort()
-    for name in names:
-        if os.path.isdir(os.path.join(path, name)) and not name.startswith('.'):
-            result.append(os.path.join(path, name))
+            pass
     return result
 
 
-def get_dirs_tree(path='.'):
+def get_filepaths(path: str = '.', ext: str = '*') -> tp.List[str]:
+    """Returns file paths list for provided path filtering by extension
+
+    :param path: (str) working directory, default - current directory
+    :param ext: (str) select by the extension (e.g. 'svg', 'ai' etc.), default - any extension
+    :return: (list) found file paths list
     """
-    Returns recursive directory path list for provided path
+    file_items = sorted(glob.glob(os.path.join(path, f'*.{ext}')))
+    return [file_item for file_item in file_items if os.path.isfile(file_item)]
+
+
+def get_dirpaths(path: str = '.', skip_hidden: bool = True) -> tp.List[str]:
+    """Returns directory paths list for provided path
+
+    :param path: (str) working directory, default - current directory
+    :param skip_hidden: (bool) skip hidden directories, e.g. '.svn'
+    :return: (list) found directory paths list
+    """
+    result = []
+    if os.path.isdir(path):
+        try:
+            for name in sorted(os.listdir(path)):
+                dirpath = os.path.join(path, name)
+                skip = name.startswith('.') and skip_hidden
+                if not skip and os.path.isdir(dirpath):
+                    result.append(dirpath)
+        except os.error:
+            pass
+    return result
+
+
+def get_dirs_tree(path: str = '.') -> tp.List[str]:
+    """Returns recursive directory paths list for provided path
+
+    :param path: (str) working directory, default - current directory
+    :return: (list) found directory paths list
     """
     tree = get_dirpaths(path)
     res = [] + tree
     for node in tree:
-        subtree = get_dirs_tree(node)
-        res += subtree
-    return res
+        res += get_dirs_tree(node)
+    return sorted(res)
 
 
-def get_files_tree(path='.', ext='*'):
-    """
-    Returns recursive file path list for provided path
+def get_files_tree(path: str = '.', ext: str = '*') -> tp.List[str]:
+    """Returns recursive file path list for provided path
+
+    :param path: (str) working directory, default - current directory
+    :param ext: (str) select by the extension (e.g. 'svg', 'ai' etc.), default - any extension
+    :return: (list) found file paths list
     """
     tree = []
-    dirs = [path, ]
-    dirs += get_dirs_tree(path)
-    for dir_item in dirs:
-        files = get_filepaths(dir_item, ext)
-        files.sort()
-        tree += files
-    return tree
+    for dir_item in [path, ] + get_dirs_tree(path):
+        tree += get_filepaths(dir_item, ext)
+    return sorted(tree)
 
 
-def normalize_path(path='.'):
+def normalize_path(path: str = '.') -> str:
+    """Returns absolute path
+
+    :param path: (str) path representation
+    :return: (str) normalized absolute path
     """
-    Returns absolute path
-    """
-    return os.path.expanduser(path) if path.startswith('~') \
-        else os.path.abspath(path)
+    return os.path.abspath(os.path.expanduser(path))
 
 
-def getsize(path='.', count=False):
-    """
-    Returns size of file or recursive size of directory
+def getsize(path='.', count=False) -> tp.Union[int, tp.Tuple[int, int]]:
+    """Returns size of file or recursive size of directory
+
+    :param path: (str) working directory, default - current directory
+    :param count: (bool) return number of files
+    :return: (int|tuple) total files size or tuple of files size and number of files
     """
     sizes = [os.path.getsize(path)] if os.path.isfile(path) \
         else [os.path.getsize(f) for f in get_files_tree(path)]
-    sz = sum(sizes)
-    return (sz, len(sizes)) if count else sz
+    return (sum(sizes), len(sizes)) if count else sum(sizes)
 
 
-def rmtree(path):
-    """
-    Recursively delete a directory tree
+def rmtree(path: str) -> None:
+    """Recursively delete a directory tree
+
+    :param path: (str) target path
     """
     if os.path.isdir(path):
         shutil.rmtree(path)
-    elif os.path.exists(path):
+    elif os.path.islink(path):
         os.unlink(path)
