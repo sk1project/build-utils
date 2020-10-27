dd940e5f1736de650ea3d526479bcf6285feddf5
sk1project <sk1.project.org@gmail.com>
2020-10-27 23:12:57 +0200

    latest ubuntu version

--- src/utils/dependencies.py
+++ src/utils/dependencies.py
@@ -17,30 +17,36 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
+import shutil
+import sys
+
 from dist import *
 
 DEB_GENERIC = 'liblcms2-2 (>=2.0), python (>=2.4), python (<<3.0), '
-DEB_GENERIC += 'python-cairo, python-reportlab, '
+DEB_GENERIC += 'python-cairo, '
 
 UC2_DEB_DEPENDENCIES = {
-    UBUNTU14: DEB_GENERIC + 'libmagickwand5, python-pil',
-    UBUNTU15: DEB_GENERIC + 'libmagickwand5, python-pil',
-    UBUNTU16: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil',
-    UBUNTU17: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil',
-    UBUNTU18: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil'
+    UBUNTU14: DEB_GENERIC + 'libmagickwand5, python-pil, python-reportlab',
+    UBUNTU15: DEB_GENERIC + 'libmagickwand5, python-pil, python-reportlab',
+    UBUNTU16: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil, python-reportlab',
+    UBUNTU17: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil, python-reportlab',
+    UBUNTU18: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil, python-reportlab'
         # Workaround for Ubuntu 18.10
         if platform.dist()[1] != '18.10' else
-        DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil',
-    UBUNTU19: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil',
+        DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil, python-reportlab',
+    UBUNTU19: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil, python-reportlab',
+    UBUNTU20: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil',
 
-    MINT17: DEB_GENERIC + 'libmagickwand5, python-pil',
-    MINT18: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil',
-    MINT19: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil',
+    MINT17: DEB_GENERIC + 'libmagickwand5, python-pil, python-reportlab',
+    MINT18: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil, python-reportlab',
+    MINT19: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil, python-reportlab',
+    MINT20: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil',
 
-    DEBIAN7: DEB_GENERIC + 'libmagickwand5, python-imaging',
-    DEBIAN8: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil',
-    DEBIAN9: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil',
-    DEBIAN10: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil',
+    DEBIAN7: DEB_GENERIC + 'libmagickwand5, python-imaging, python-reportlab',
+    DEBIAN8: DEB_GENERIC + 'libmagickwand-6.q16-2, python-pil, python-reportlab',
+    DEBIAN9: DEB_GENERIC + 'libmagickwand-6.q16-3, python-pil, python-reportlab',
+    DEBIAN10: DEB_GENERIC + 'libmagickwand-6.q16-6, python-pil, python-reportlab',
 }
 
 SK1_DEB_DEPENDENCIES = {
@@ -50,10 +56,12 @@ SK1_DEB_DEPENDENCIES = {
     UBUNTU17: 'python-wxgtk3.0, python-cups',
     UBUNTU18: 'python-wxgtk3.0, python-cups',
     UBUNTU19: 'python-wxgtk3.0, python-cups',
+    UBUNTU20: 'python-wxgtk3.0, libcups2',
 
     MINT17: 'python-wxgtk2.8, python-cups',
     MINT18: 'python-wxgtk3.0, python-cups',
     MINT19: 'python-wxgtk3.0, python-cups',
+    MINT20: 'python-wxgtk3.0, libcups2',
 
     DEBIAN7: 'python-wxgtk2.8, python-cups',
     DEBIAN8: 'python-wxgtk3.0, python-cups',
@@ -109,6 +117,17 @@ SK1_RPM_DEPENDENCIES = {
     OPENSUSE15_1: 'python-wxWidgets python2-pycups',
 }
 
+UC2_PIP_DEPENDENCIES = {
+    UBUNTU20: ['reportlab-3.5.53.tar.gz'],
+    MINT20: ['reportlab-3.5.53.tar.gz']
+}
+
+SK1_PIP_DEPENDENCIES = {
+    # libcups2-dev
+    UBUNTU20: ['pycups-1.9.74.tar.gz'],
+    MINT20: ['pycups-1.9.74.tar.gz']
+}
+
 
 def get_uc2_deb_depend():
     if SYSFACTS.sid in UC2_DEB_DEPENDENCIES:
@@ -144,3 +163,36 @@ def get_sk1_rpm_depend():
     elif uc2_dep:
         sk1_dep = uc2_dep
     return sk1_dep
+
+
+def install_pip_deps(deps):
+    if os.path.exists('setup.cfg'):
+        os.system('mv setup.cfg setup.cfg_')
+    py_version = '.'.join(sys.version.split()[0].split('.')[:2])
+    machine = platform.machine()
+    target = './build/lib.linux-%s-%s' % (machine, py_version)
+    for item in deps:
+        pkg = './utils/packages/%s' % item
+        if os.system('pip2 install --target %s %s' % (target, pkg)):
+            raise Exception('Error installing %s' % item)
+    artifacts = [item for item in os.listdir(target) if item.endswith('-info')]
+    artifacts += ['PIL']
+    for item in artifacts:
+        item = os.path.join(target, item)
+        if os.path.exists(item):
+            shutil.rmtree(item)
+    if os.path.exists('setup.cfg_'):
+        os.system('mv setup.cfg_ setup.cfg')
+
+
+def install_uc2_pip_deps():
+    deps = UC2_PIP_DEPENDENCIES.get(SYSFACTS.sid)
+    if deps:
+        install_pip_deps(deps)
+
+
+def install_sk1_pip_deps():
+    deps = UC2_PIP_DEPENDENCIES.get(SYSFACTS.sid, []) + \
+           SK1_PIP_DEPENDENCIES.get(SYSFACTS.sid, [])
+    if deps:
+        install_pip_deps(deps)
